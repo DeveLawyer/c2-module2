b2867f5d0c78ac5e9deee2ae011aa2bb8d39e717
Josue Narro <josue.narro.a@gmail.com>
2020-04-13 17:36:10 -0500

    Absolute sum done

---
+++ week-1/day-1/absolute-sum.rb
@@ -0,0 +1,22 @@
+=begin
+    Integer(num) will throw an ArgumentError exception if num isn't a valid integer (you can specify the base): Integer("2hi") // ArgumentError
+
+    num.to_i will convert as much as it can: "2hi".to_i // 2
+=end
+
+print "Enter the first number: "
+a = Integer(gets.chomp)
+print "Enter the second number: "
+b = Integer(gets.chomp)
+
+# Checks if the first number is negative
+if a < 0
+    a = a + ((a * -1) * 2)
+end
+
+# Checks if the second number is (also) negative
+if b < 0
+    b = b + ((b * -1) * 2)
+end
+
+puts "The absolute value of the sum is #{a + b}"
