d68c5f22b97af99664512478c7ecfbe59f545a88
Josué Zamir Narro Abanto <52726455+DeveLawyer@users.noreply.github.com>
2020-04-13 23:50:38 -0500

    Fix indentation

--- week-1/day-1/longest-string.rb
+++ week-1/day-1/longest-string.rb
@@ -1,5 +1,5 @@
 def longest_string(string_a, string_b)
-    string_a.length > string_b.length ? (puts string_a) : (puts string_b)
+  string_a.length > string_b.length ? (puts string_a) : (puts string_b)
 end
 
 longest_string("I am the longest.", "No, you are not the longest; liar.")
