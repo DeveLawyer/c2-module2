614b3b1d09d28e4a97162b9615038a7772030b42
Josue Narro <josue.narro.a@gmail.com>
2020-04-14 13:03:19 -0500

    Min and max done

---
+++ week-1/day-2/min-max.rb
@@ -0,0 +1,19 @@
+def max(numbers)
+    highest_so_far = nil.to_i
+    numbers.each { |number|
+        number > highest_so_far ? highest_so_far = number : nil 
+    }
+    puts "The maximum number is #{highest_so_far}"
+end
+
+def min(numbers)
+    lowest_so_far = nil.to_i
+    numbers.each { |number|
+        number < lowest_so_far ? lowest_so_far = number : nil 
+    }
+    puts "The minimum number is #{lowest_so_far}"
+end
+
+numbers = [2, 56, 5, 3, 4, 6, 1, -3456, 123891, 454, 1, 23, 5, 7]
+min(numbers) # Result is -3456
+max(numbers) # Result is 123891
