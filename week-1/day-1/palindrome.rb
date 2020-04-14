0e4d4cbc2a401d500f4598a7d93abdb8e98d4bd2
Josué Zamir Narro Abanto <52726455+DeveLawyer@users.noreply.github.com>
2020-04-13 23:52:16 -0500

    Fix indentation

--- week-1/day-1/palindrome.rb
+++ week-1/day-1/palindrome.rb
@@ -1,6 +1,6 @@
 def palindrome?(sentence)
-    no_spaces = sentence.gsub(" ", "").downcase
-    no_spaces == no_spaces.reverse ? true : false
+  no_spaces = sentence.gsub(" ", "").downcase
+  no_spaces == no_spaces.reverse ? true : false
 end
 
 puts palindrome?("Never odd or even")
