d5d67945ed0075c4b452536e4d4e320423cab81e
Josue Narro <josue.narro.a@gmail.com>
2020-04-14 15:40:34 -0500

    Refactor solution

--- week-1/day-2/sum-of-cubes.rb
+++ week-1/day-2/sum-of-cubes.rb
@@ -1,15 +1,8 @@
 def sum_of_cubes(a, b)
-  # Creates an array of cubes for the range of numbers 
-  arr_num = []
-  for num in a..b
-    arr_num.push(num ** 3)
-  end
-  
-  # Sums the elements
   sum = 0
-  arr_num.each { |num|
-    sum += num
-  }
+  for num in a..b
+    sum += num ** 3
+  end
   sum
 end
 
