3a6d7cc6f3fbea8c984bec809225328328eee958
Josué Zamir Narro Abanto <52726455+DeveLawyer@users.noreply.github.com>
2020-04-13 23:45:45 -0500

    Fix indentation

--- week-1/day-1/frequency-finder.rb
+++ week-1/day-1/frequency-finder.rb
@@ -1,6 +1,6 @@
 def find_frequency(sentence, word)
-    array_of_words = sentence.downcase.split(" ")
-    array_of_words.count(word)
+  array_of_words = sentence.downcase.split(" ")
+  array_of_words.count(word)
 end
 
 puts find_frequency('Ruby is The best language in the World', 'the')
