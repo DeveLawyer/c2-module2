b86283d7063f6c2960444e9f429d022d96f40425
Josué Zamir Narro Abanto <52726455+DeveLawyer@users.noreply.github.com>
2020-04-13 23:53:56 -0500

    Fix indentation

--- week-1/day-1/ideal-gas-law.rb
+++ week-1/day-1/ideal-gas-law.rb
@@ -2,7 +2,7 @@
 IDEAL_GAS = 8.314
 
 def calculate_pressure(chemical_amount, temperature, volume)
-    puts (chemical_amount * IDEAL_GAS * temperature) / volume
+  puts (chemical_amount * IDEAL_GAS * temperature) / volume
 end
 
 calculate_pressure(5, 500, 2) # Returns 10392.5
