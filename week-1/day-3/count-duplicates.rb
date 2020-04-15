1127a95136badc6b8c0f28d74ab53d496b29c8a9
Josue Narro <josue.narro.a@gmail.com>
2020-04-15 16:41:19 -0500

    Count duplicates done

---
+++ week-1/day-3/count-duplicates.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+def count_duplicates(numbers)
+  visited = []
+  duplicates = 0
+  numbers.sort!.each do |num|
+    visited.include?(num) ? duplicates += 1 : visited << num
+  end
+  duplicates
+end
+
+numbers_1 = [1, 2, 3, 2, 5, 8, 5, 10, 5]
+numbers_2 = [17, 13, 19, 17, 19, 7, 10, 11, 5, 11, 18, 13, 3, 12, 19, 0, 17, 1, 12, 12, 5, 12, 1, 1, 0, 14, 5, 4, 9, 9, 3, 10, 9, 9, 12, 16, 9, 17, 12, 11, 2, 2, 14, 8, 11, 12, 7, 1, 16, 2, 14]
+
+p count_duplicates(numbers_1)
+# 3
+p count_duplicates(numbers_2)
+# 33
