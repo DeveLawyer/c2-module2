3f4522f4dce9c9a6b5042c047bf009e1c88577ec
Josue Narro <josue.narro.a@gmail.com>
2020-04-15 11:54:46 -0500

    Linting code

--- week-1/day-2/whos-online.rb
+++ week-1/day-2/whos-online.rb
@@ -1,26 +1,28 @@
+# frozen_string_literal: true
+
 def who_is_online(arr)
   status_hash = Hash.new { |hash, prop| hash[prop] = [] }
   arr.each do |person|
-    if person["last_activity"] > 10 && person["status"] != "offline"
-      person["status"] = "away"
+    if person['last_activity'] > 10 && person['status'] != 'offline'
+      person['status'] = 'away'
     end
-  
-    case person["status"]
-      when "online"
-        status_hash["online"] << person["username"]
-      when "offline"
-        status_hash["offline"] << person["username"]
-      when "away"
-        status_hash["away"] << person["username"]
+
+    case person['status']
+    when 'online'
+      status_hash['online'] << person['username']
+    when 'offline'
+      status_hash['offline'] << person['username']
+    when 'away'
+      status_hash['away'] << person['username']
     end
   end
   status_hash
 end
 
 friends = [
-  {"username"=> "David", "status"=> "online", "last_activity"=> 10},
-  {"username"=> "Lucy", "status"=> "offline", "last_activity"=> 22},
-  {"username"=> "Bob", "status"=> "online", "last_activity"=> 104}
+  { 'username' => 'David', 'status' => 'online', 'last_activity' => 10 },
+  { 'username' => 'Lucy', 'status' => 'offline', 'last_activity' => 22 },
+  { 'username' => 'Bob', 'status' => 'online', 'last_activity' => 104 }
 ]
 
 who_is_online(friends)
