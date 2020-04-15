d491b099b3bc6332ab5a0213995cb6c4ce3faa6a
Josue Narro <josue.narro.a@gmail.com>
2020-04-14 19:18:00 -0500

    Fix indentation

--- week-1/day-2/min-max.rb
+++ week-1/day-2/min-max.rb
@@ -1,17 +1,17 @@
 def max(numbers)
-    highest_so_far = numbers[0]
-    numbers.each { |number|
-        number > highest_so_far ? highest_so_far = number : nil 
-    }
-    puts "The maximum number is #{highest_so_far}"
+  highest_so_far = numbers[0]
+  numbers.each { |number|
+    number > highest_so_far ? highest_so_far = number : nil 
+  }
+  puts "The maximum number is #{highest_so_far}"
 end
 
 def min(numbers)
-    lowest_so_far = numbers[0]
-    numbers.each { |number|
-        number < lowest_so_far ? lowest_so_far = number : nil 
-    }
-    puts "The minimum number is #{lowest_so_far}"
+  lowest_so_far = numbers[0]
+  numbers.each { |number|
+    number < lowest_so_far ? lowest_so_far = number : nil 
+  }
+  puts "The minimum number is #{lowest_so_far}"
 end
 
 numbers = [2, 56, 5, 3, 4, 6, 1, -3456, 123891, 454, 1, 23, 5, 7]
