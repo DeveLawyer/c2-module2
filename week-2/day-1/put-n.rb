abe8484df196b8e34732b27bfa9e540a4f37299d
Josue Narro <josue.narro.a@gmail.com>
2020-04-20 20:43:58 -0500

    Fix indentation

--- week-2/day-1/put-n.rb
+++ week-2/day-1/put-n.rb
@@ -1,11 +1,13 @@
-def put_n(string, n)
-    if !n.is_a?(Integer)
-        raise ArgumentError.new("Not an integer")
-    elsif !n.positive?
-        raise ArgumentError.new("Not a positive integer")
-    end
+# frozen_string_literal: true
 
-    n.times { puts string }
+def put_n(string, n)
+  if !n.is_a?(Integer)
+    raise ArgumentError, 'Not an integer'
+  elsif !n.positive?
+    raise ArgumentError, 'Not a positive integer'
+  end
+
+  n.times { puts string }
 end
 
-put_n("Repeat me", 10)
+put_n('Repeat me', 10)
