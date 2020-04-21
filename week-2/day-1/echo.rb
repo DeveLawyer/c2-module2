5075e4c0e86f898b5cd74d781ff1735047809312
Josue Narro <josue.narro.a@gmail.com>
2020-04-20 20:42:41 -0500

    Fix indentation

--- week-2/day-1/echo.rb
+++ week-2/day-1/echo.rb
@@ -1,20 +1,22 @@
-def put_n(string, n)
-    if !n.is_a?(Integer)
-        raise ArgumentError.new("Not an integer")
-    elsif !n.positive?
-        raise ArgumentError.new("Not a positive integer")
-    end
+# frozen_string_literal: true
 
-    n.times { puts string }
+def put_n(string, n)
+  if !n.is_a?(Integer)
+    raise ArgumentError, 'Not an integer'
+  elsif !n.positive?
+    raise ArgumentError, 'Not a positive integer'
+  end
+
+  n.times { puts string }
 end
 
 begin
-    print "What do you want to echo? "
-    message = gets.chomp
-    print "How many times do you want to repeat it? "
-    repeat = Integer(gets.chomp)
-    put_n(message, repeat)
+  print 'What do you want to echo? '
+  message = gets.chomp
+  print 'How many times do you want to repeat it? '
+  repeat = Integer(gets.chomp)
+  put_n(message, repeat)
 rescue ArgumentError
-    p "I don't understand how many times do you want to echo the string!"
-    retry
+  p "I don't understand how many times do you want to echo the string!"
+  retry
 end
