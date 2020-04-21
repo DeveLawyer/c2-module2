5400bcaf0db9485a6d71eaa550b08299fe2966c1
Josue Narro <josue.narro.a@gmail.com>
2020-04-20 20:20:29 -0500

    Refactor check positive statement

--- week-2/day-1/echo.rb
+++ week-2/day-1/echo.rb
@@ -1,7 +1,7 @@
 def put_n(string, n)
     if !n.is_a?(Integer)
         raise ArgumentError.new("Not an integer")
-    elsif n < 0
+    elsif !n.positive?
         raise ArgumentError.new("Not a positive integer")
     end
 
